db54e1d9d403aae64026080dd29aeab295e8b0c9
Chad Freeman <ceo@marinadev.xyz>
2024-05-20 14:43:57 -0400

    Add autofocus to Modal

--- game/src/lib/Modal.svelte
+++ game/src/lib/Modal.svelte
@@ -1,14 +1,22 @@
-<script>
+<script lang=ts>
     import { game } from "./state";
+    let button: HTMLButtonElement;
+    onMount(() => {
+        game.subscribe(g => {
+            if(g.modal !== false) {
+                button.focus()
+            }
+        })
+    })
 </script>
 
 <input type="checkbox" checked={$game.modal !== false} class="modal-toggle" />
 <div class="modal" role="dialog">
     <div class="modal-box">
         <h3 class="font-bold text-lg" class:text-success={$game.modal=="pass"} class:text-error={$game.modal=="fail"}>{#if $game.modal == "pass"}You won!{:else if $game.modal == "fail"}You Failed{/if}</h3>
-        <p class="py-4"></p>
+        <p class="py-4">The answer was {$game.answer}</p>
         <div class="modal-action">
-            <button on:click={() => game.reset()} disabled={$game.modal == false} class="btn">Ok</button>
+            <button bind:this={button} on:click={() => game.reset()} disabled={$game.modal == false} class="btn">Ok</button>
         </div>
     </div>
 </div>
